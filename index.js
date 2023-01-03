1dd5421b68dd02e4a8a4bfacf8a96204845136be
Tran Sang <me@transa.ng>
2023-01-03 20:07:45 +0900

    init commit

---
+++ index.js
@@ -0,0 +1,93 @@
+'use strict'
+Object.defineProperty(exports, '__esModule', {value: true})
+exports.greektheta = exports.greekTheta = exports.greeketa = exports.greekEta = exports.greekzeta = exports.greekZeta = exports.greekepsilon = exports.greekEpsilon = exports.greekdelta = exports.greekDelta = exports.greekgamma = exports.greekGamma = exports.greekbeta = exports.greekBeta = exports.greekalpha = exports.greekAlpha = exports.greaterThan = exports.lessThan = exports.geq = exports.leq = exports.notEqual = exports.almostEqual = exports.trademark = exports.euroSign = exports.doublePrime = exports.prime = exports.ellipsis = exports.bullet = exports.doubleDagger = exports.singleDagger = exports.doubleQuoteLow = exports.singleQuoteLow = exports.emDash = exports.enDash = exports.fractionalHalf = exports.middleDot = exports.pilcrow = exports.plusMinus = exports.degree = exports.registeredTrademark = exports.guillemetsRight = exports.guillemetsLeft = exports.copyright = exports.section = exports.pound = exports.cent = exports.doubleQuoteRight = exports.doubleQuoteLeft = exports.singleQuoteRight = exports.singleQuoteLeft = void 0
+exports.greekomega = exports.greekOmega = exports.greekpsi = exports.greekPsi = exports.greekchi = exports.greekChi = exports.greekphi = exports.greekPhi = exports.greekupsilon = exports.greekUpsilon = exports.greektau = exports.greekTau = exports.greeksigma = exports.greekSigma = exports.greekrho = exports.greekRho = exports.greekpi = exports.greekPi = exports.greekomicron = exports.greekOmicron = exports.greekxi = exports.greekXi = exports.greeknu = exports.greekNu = exports.greekmu = exports.greekMu = exports.greeklambda = exports.greekLambda = exports.greekkappa = exports.greekKappa = exports.greekiota = exports.greekIota = void 0
+// https://www.w3.org/wiki/Common_HTML_entities_used_for_typography
+exports.singleQuoteLeft = '‘' // &lsquo; or &#8216;    ' or &apos;
+exports.singleQuoteRight = '’' // &rsquo; or &#8217; apostrophe, Smart Quotes
+exports.doubleQuoteLeft = '“' // &ldquo; or &#8220; Smart Quotes,  ", &quot;
+exports.doubleQuoteRight = '”' // &rdquo; or &#8221; Smart Quotes, ``
+// uncommon
+// Cent (currency)
+exports.cent = '¢' // &cent; or &#162;
+// Pound (currency)
+exports.pound = '£' // &pound; or &#163;
+exports.section = '§' // &sect; or &#167;
+exports.copyright = '©' // &copy; or &#169; or (c)
+exports.guillemetsLeft = '«' // &laquo; or &#171;
+exports.guillemetsRight = '»' // &raquo; or &#187; &quot;
+exports.registeredTrademark = '®' //   &reg; or    &#174;  or (R)
+exports.degree = '°' //    &deg;   or &#176;
+exports.plusMinus = '±' // &plusmn; or &#177; or +/-
+exports.pilcrow = '¶' // &para; or &#182; // paragraph
+exports.middleDot = '·' // &middot;    or &#183;
+exports.fractionalHalf = '½' // &frac12;   or &#188;   or 1/2
+exports.enDash = '–' //    &ndash; or &#8211; or   - for ranges
+// long dash
+exports.emDash = '—' //    &mdash; or &#8212; or   - enclosed by spaces, or --
+exports.singleQuoteLow = '‚' //    &sbquo; or &#8218;  ' or comma
+exports.doubleQuoteLow = '„' //    &bdquo; or &#8222;  &quot; or ,,
+exports.singleDagger = '†' // &dagger; or &#8224; or *
+exports.doubleDagger = '‡' // &Dagger;  &#8225;     or **
+exports.bullet = '•' //    &bull;  or &#8226; or   *
+exports.ellipsis = '…' //  &hellip;    or &#8230;  or ...
+exports.prime = '′' // &prime; or &#8242; or ', &apos; // minute
+exports.doublePrime = '″' //    &Prime;    or  &#8243; or &quot;, // second, inch
+exports.euroSign = '€' //  &euro;  or &#8364;
+exports.trademark = '™' // &trade; or &#8482;  or (tm)
+exports.almostEqual = '≈' //   &asymp; or &#8776;  or ~
+exports.notEqual = '≠' //  &ne;    or &#8800;  or !=
+exports.leq = '≤' // &le; or &#8804; or <=
+exports.geq = '≥' //    &ge;   or &#8805;  or >=
+exports.lessThan = '<' // &lt; or &#062;
+exports.greaterThan = '>' // &gt;  or  &#060;
+// https://en.wikipedia.org/wiki/Greek_alphabet
+// greek letters
+exports.greekAlpha = 'Α'
+exports.greekalpha = 'α'
+exports.greekBeta = 'Β'
+exports.greekbeta = 'β'
+exports.greekGamma = 'Γ'
+exports.greekgamma = 'γ'
+exports.greekDelta = 'Δ'
+exports.greekdelta = 'δ'
+exports.greekEpsilon = 'Ε'
+exports.greekepsilon = 'ε'
+exports.greekZeta = 'Ζ'
+exports.greekzeta = 'ζ'
+exports.greekEta = 'Η'
+exports.greeketa = 'η'
+exports.greekTheta = 'Θ'
+exports.greektheta = 'θ'
+exports.greekIota = 'Ι'
+exports.greekiota = 'ι'
+exports.greekKappa = 'Κ'
+exports.greekkappa = 'κ'
+exports.greekLambda = 'Λ'
+exports.greeklambda = 'λ'
+exports.greekMu = 'Μ'
+exports.greekmu = 'μ'
+exports.greekNu = 'Ν'
+exports.greeknu = 'ν'
+exports.greekXi = 'Ξ'
+exports.greekxi = 'ξ'
+exports.greekOmicron = 'Ο'
+exports.greekomicron = 'ο'
+exports.greekPi = 'Π'
+exports.greekpi = 'π'
+exports.greekRho = 'Ρ'
+exports.greekrho = 'ρ'
+exports.greekSigma = 'Σ'
+exports.greeksigma = 'σ' // or ς
+exports.greekTau = 'Τ'
+exports.greektau = 'τ'
+exports.greekUpsilon = 'Υ'
+exports.greekupsilon = 'υ'
+exports.greekPhi = 'Φ'
+exports.greekphi = 'φ'
+exports.greekChi = 'Χ'
+exports.greekchi = 'χ'
+exports.greekPsi = 'Ψ'
+exports.greekpsi = 'ψ'
+exports.greekOmega = 'Ω'
+exports.greekomega = 'ω'
